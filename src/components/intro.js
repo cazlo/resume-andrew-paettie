1da577db37083865139ea9dc3e64ee6b3b316e79
Drew Paettie <cazlo@users.noreply.github.com>
2017-08-27 17:35:28 -0500

    better landing page w/image + css theme

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,15 +1,32 @@
 import { h } from 'preact';
 // eslint-disable-next-line
-import { Container, Row, Col, Button, Navbar, Nav, NavItem, NavLink , Jumbotron} from 'reactstrap';
+import { Container, Row, Col, Button, Navbar, Nav, NavItem, NavLink , Jumbotron, Media} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const Intro = (props) => (
   <div className='container'>
-    <Jumbotron>
-      <h1 className='display-3'>Hello, world!</h1>
-      <p className='lead'>My name is Andrew Paettie. I am an experienced and capable full stack software engineer.</p>
-      <hr className='my-2' />
-      <p>If you are interested in hiring a full stack software engineer, check out my <Link to='/resume'>résumé</Link> and <Link to='/projects'>projects</Link></p>
+    <Jumbotron >
+      <Media>
+        <Row className='justify-content-center'>
+
+          <Col xs='12' sm='6' lg='8' className='justify-content-center'>
+            <Media body >
+              <Media heading>
+                <h2>About Me</h2>
+              </Media>
+              <p className='lead'>
+          I am an experienced and capable full stack software engineer.
+          If you are interested in hiring a full stack software engineer,
+                  check out my <Link to='/resume'>résumé</Link> and <Link to='/projects'>projects</Link>
+              </p>
+            </Media>
+          </Col>
+          <Col xs='12' sm='6' md='6' lg='4'>
+            <img src='/img/andrew-paettie-having-fun.jpg' alt='Andrew Paettie' className='img-thumbnail' />
+          </Col>
+        </Row>
+      </Media>
+      <p className='lead' />
     </Jumbotron>
   </div>
 );
