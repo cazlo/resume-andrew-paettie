0eba52db84697728f4564f2abbc10acd9ca5625a
Drew Paettie <cazlo@users.noreply.github.com>
2017-08-27 19:13:49 -0500

    lint

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,8 +17,8 @@ const ProjectCard = (props) => (
       <CardSubtitle>{props.subtitle}</CardSubtitle>
       <CardText><p>{props.text}</p></CardText>
       <ButtonGroup>
-        {props.githubUrl? (<a href={props.githubUrl}><Button tag="a" className='btn btn-outline-info'><FontAwesome name='github' className='text-dark' /> Github</Button></a>) : null}
-        {props.siteUrl? (<a href={props.siteUrl}><Button tag="a" className='btn btn-outline-info'><FontAwesome name='hashtag'className='text-dark' /> Site</Button></a>) : null}
+        {props.githubUrl? (<a href={props.githubUrl}><Button tag='a' className='btn btn-outline-info'><FontAwesome name='github' className='text-dark' /> Github</Button></a>) : null}
+        {props.siteUrl? (<a href={props.siteUrl}><Button tag='a' className='btn btn-outline-info'><FontAwesome name='hashtag'className='text-dark' /> Site</Button></a>) : null}
       </ButtonGroup>
     </CardBlock>
   </Card>
