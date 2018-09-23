0d31b167f9366ba1c3df338b59e4f57162901152
Andrew Paettie <paettiea@gmail.com>
2018-09-22 20:28:23 -0500

    snazzy new font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+import { render } from 'react-dom';
+import WebFont from 'webfontloader';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+WebFont.load({
+  google: {
+    families: [
+      'Material Icons',
+      'Roboto:400,500',
+      'Open Sans:300italic,400italic,600italic,700italic,800italic,400,600,700,800,300',
+    ],
+  },
+});
 
 render(
   <BrowserRouter>
