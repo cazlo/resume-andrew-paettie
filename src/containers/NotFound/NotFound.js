b2772a0c54c874510209ae804aa28e325b0a7c54
Andrew Paettie <paettiea@gmail.com>
2018-10-05 04:54:16 -0500

    Make this look a little better

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 import ScreenBlock from '../../components/ScreenBlock/ScreenBlock';
 import SnakeGame from '../SnakeGame/SnakeGame';
+import techTheme from '../../common/techTheme';
+
+import './NotFound.css';
 
 class NotFound extends Component {
   componentDidMount() {}
@@ -11,8 +16,24 @@ class NotFound extends Component {
 
   render() {
     return (
-      <ScreenBlock>
-        <SnakeGame />
+      <ScreenBlock className="NotFoundBlock">
+        <div className="container">
+          <SnakeGame />
+
+          <Paper className="NotFoundPaper">
+            <div className="heading">
+              <h2>404 Not Found</h2>
+            </div>
+            <Button
+              variant="contained"
+              target="_blank"
+              href="/"
+              style={{ ...techTheme.android.style, marginBottom: '15px' }}
+            >
+              Resume
+            </Button>
+          </Paper>
+        </div>
       </ScreenBlock>
     );
   }
