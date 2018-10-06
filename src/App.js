726ba3d8b5acdc9fd6573ee16522e6b80f55c85d
Andrew Paettie <paettiea@gmail.com>
2018-10-06 13:29:44 -0500

    better organization/style of not found

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import blue from '@material-ui/core/colors/blue';
 import red from '@material-ui/core/colors/red';
 
 import Resume from './containers/Resume/Resume';
-import NotFound from './containers/NotFound/NotFound';
+import NotFound from './components/NotFound/NotFound';
 import Copyright from './components/Copyright/Copyright';
 
 const theme = createMuiTheme({
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import { FaHome } from 'react-icons/fa';
+
+import ScreenBlock from '../ScreenBlock/ScreenBlock';
+import SnakeGame from '../../containers/SnakeGame/SnakeGame';
+import techTheme from '../../common/techTheme';
+
+const styles = theme => ({
+  NotFoundPaper: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    width: '100%',
+    textAlign: 'center',
+    marginBottom: '15px',
+    backgroundColor: '#eeeeee',
+  },
+  NotFoundButton: {
+    ...techTheme.android.style,
+    marginBottom: '15px',
+  },
+});
+
+const NotFound = props => {
+  const { classes } = props;
+
+  return (
+    <ScreenBlock className="NotFoundBlock">
+      <div className="container">
+        <Paper className={classes.NotFoundPaper}>
+          <div className="heading">
+            <h2>404 Not Found</h2>
+          </div>
+          <Button
+            component={Link}
+            variant="contained"
+            target="_blank"
+            to="/"
+            className={classes.NotFoundButton}
+          >
+            <FaHome size="2em" />
+          </Button>
+        </Paper>
+        <SnakeGame />
+      </div>
+    </ScreenBlock>
+  );
+};
+
+NotFound.propTypes = {
+  classes: PropTypes.object,
+};
+
+export default withStyles(styles)(NotFound);
