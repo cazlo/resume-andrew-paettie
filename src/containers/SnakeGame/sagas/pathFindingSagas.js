835946316ecda8b0aa9a53a845e2a10eae0a37af
Drew Paettie <cazlo@users.noreply.github.com>
2018-11-15 09:42:17 -0600

    make the AI a little bit less suicidal

--- src/containers/SnakeGame/sagas/pathFindingSagas.js
+++ src/containers/SnakeGame/sagas/pathFindingSagas.js
@@ -1,36 +1,67 @@
-import Direction from '../util/Direction';
 import _ from 'lodash';
-import GridState from '../util/GridState';
-import PositionUtil from '../util/PositionUtil';
-import Easystarjs from 'easystarjs';
 import {  put, select, take, takeLatest } from 'redux-saga/effects';
-import { changeDirection } from '../actions/gameAction';
+import Easystarjs from 'easystarjs';
+
 import Action from '../actions/Action';
+import { changeDirection } from '../actions/gameAction';
 import { finishPathFind, pathNotFound, startPathFind, ignoringStaleResult } from '../actions/pathFindingAction';
+import Direction from '../util/Direction';
+import GridState from '../util/GridState';
+import Position from '../util/Position';
+import PositionUtil from '../util/PositionUtil';
+
+// eslint-disable-next-line new-cap
+const aStar = new Easystarjs.js();
 
 // todo the return values here are not actually used, only yield. this is weird
-function* moveFromPath (path, head) {
+function* moveFromPath (path, snake, snakeDirection) {
   if (!path || (path && path.length === 0)) {
     return false;
   }
+  const head = snake[0];
   const firstMove = path[1] || path[0]; // idx 0 is usually the current node the snake is on
-  let directionToMove = null;
+  let directionsToMove = [];
   if (firstMove.x !== head.x) {
     if (firstMove.x < head.x) {
-      directionToMove = Direction.LEFT;
+      directionsToMove.push(Direction.LEFT);
     } else {
-      directionToMove = Direction.RIGHT;
-    }
-  } else if (firstMove.y !== head.y) {
-    if (firstMove.y < head.y) {
-      directionToMove = Direction.UP;
-    } else {
-      directionToMove = Direction.DOWN;
+      directionsToMove.push(Direction.RIGHT);
     }
   }
-  if (directionToMove) {
-    yield put(changeDirection(directionToMove));
-    return true;
+  if (firstMove.y !== head.y) {
+    if (firstMove.y < head.y) {
+      directionsToMove.push(Direction.UP);
+    } else {
+      directionsToMove.push(Direction.DOWN);
+    }
+  }
+  if (directionsToMove.length) {
+    // don't allow the snake to make a move that would eat itself
+    if (snake.length > 1) {
+      const validDs = directionsToMove.filter(d => !PositionUtil.oppositeDirection(d, snakeDirection));
+      for (const directionToMove of validDs) {
+        if (!validDs.length) {
+          // console.log("No valid non-opposite direction moves found for path");
+          yield put(ignoringStaleResult({ currentHead: head, direction: snakeDirection, pathHead: path[0] }));
+          continue;
+        }
+        const newHeadPosition = Position(head.x + directionToMove.x, head.y + directionsToMove.y);
+        const collisions = snake.filter(s => PositionUtil.isSamePosition(s, newHeadPosition));
+        if (collisions.length) {
+          // console.log("About to make move that would cause collision");
+          yield put(ignoringStaleResult({ currentHead: head, direction: snakeDirection, pathHead: path[0] }));
+        }else  if (PositionUtil.isSamePosition(snakeDirection, directionsToMove)) {
+          // console.log("Ignore NOOP change direction");
+        } else {
+          yield put(changeDirection(directionToMove));
+        }
+      }
+    } else {
+      // not any need to do collision detection or avoid reversing direction if the snake's length
+      // is exactly 1 (moving just a head has no restrictions within the game)
+      yield put(changeDirection(directionsToMove[0]));
+    }
+
   }
   // console.warn('Path is noop somehow');
   return false;
@@ -54,8 +85,7 @@ const pathFindPromise = ({
     nodesSurroundingSnakeCostMultiplier, normalNodeCostMultiplier
 }) => new Promise(resolve => {
   const head = newSnake[0];
-  // eslint-disable-next-line new-cap
-  const aStar = new Easystarjs.js();
+
   aStar.setGrid(newGrid);
   const allowedStates = [GridState.WALKABLE, GridState.WALKABLE_PENALTY];
   aStar.setAcceptableTiles(allowedStates);
@@ -95,7 +125,7 @@ function* pathfind (newSnake, food, numRows, numCols, aiConfig){
   // | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 | 0 | 0 | 0 | 0 |
   // | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 | 0 | 0 | 0 | 0 |
   // so basically add 4 identical maps around each side of the original play area and make the
-  // diagonals enterable
+  // diagonals non-enterable
 
   const grid = _.map(_.range(numRows), () =>
     _.map(_.range(numCols), () => GridState.WALKABLE),
@@ -117,11 +147,12 @@ function* pathfind (newSnake, food, numRows, numCols, aiConfig){
 export function* pathFindMover(action) {
   const s = yield select();
   const currentHead = s.game.snake.parts[0];
+  const direction = s.game.snake.direction;
   const pathHead = action.payload[0];
   if (s.pathFinding.path.length && (pathHead.x !== currentHead.x && pathHead.y !== currentHead.y)){
-    yield put(ignoringStaleResult());
+    yield put(ignoringStaleResult({currentHead, direction, pathHead}));
   } else {
-    yield moveFromPath(s.pathFinding.path, currentHead);
+    yield moveFromPath(s.pathFinding.path, s.game.snake.parts, direction);
   }
 }
 
