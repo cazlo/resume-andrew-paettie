2d2e27b8da0722f461eb0972b676f06668ed0221
Andrew Paettie <paettiea@gmail.com>
2018-11-17 12:59:45 -0600

    add int test coverage for both current pathfinding schemes

--- src/containers/SnakeGame/sagas/performance10x10.int.js
+++ src/containers/SnakeGame/sagas/performance10x10.int.js
@@ -1,6 +1,8 @@
 import { performanceTest } from './testHelper';
+import { toggleEnableAstar, toggleGreedyShortestPathToTail } from '../actions/aiConfigAction';
 
 // 12 % avg seems to be a bit better than current performance, so should be able to be used to spot
 // performance regressions
-performanceTest({ gamesToSimulate: 20, avgThreshold: 0.25, size:10 });
+performanceTest({ gamesToSimulate: 20, avgThreshold: 0.25, size:10, aiAction: toggleEnableAstar, name:"A star" });
+performanceTest({ gamesToSimulate: 20, avgThreshold: 0.25, size:10, aiAction: toggleGreedyShortestPathToTail, name:"Greedy SP->Tail" });
 
--- src/containers/SnakeGame/sagas/performance8x8.int.js
+++ src/containers/SnakeGame/sagas/performance8x8.int.js
@@ -1,6 +1,8 @@
 import { performanceTest } from './testHelper';
+import { toggleGreedyShortestPathToTail, toggleEnableAstar } from '../actions/aiConfigAction'
 
 // 12 % avg seems to be a bit better than current performance, so should be able to be used to spot
 // performance regressions
-performanceTest({ gamesToSimulate: 30, avgThreshold: 0.38, size:8 });
+performanceTest({ gamesToSimulate: 30, avgThreshold: 0.38, size:8, aiAction: toggleEnableAstar, name:"A star" });
+performanceTest({ gamesToSimulate: 30, avgThreshold: 0.38, size:8, aiAction: toggleGreedyShortestPathToTail, name:"Greedy SP->Tail" });
 
