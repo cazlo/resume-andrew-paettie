a3071535044962758d2daf9f90f964c7870d8144
Andrew Paettie <paettiea@gmail.com>
2018-11-17 09:31:03 -0600

    add greedy path finding algorithm - shortest path to tail    this should help to avoid some basic traps as outlined in int tests    it is not optimal yet in that it should probably wander around more    instead of going directyl to tail

---
+++ src/containers/SnakeGame/sagas/greedyPathFinding.test.js
@@ -0,0 +1,31 @@
+import { pathfindGreedy } from './pathFindingSagas';
+import Position from '../util/Position';
+const p = Position;// alias because 'Position' it is long
+
+describe('Path finding trap avoidance',() => {
+  [
+    {
+      name: "5x4 approaching trap",
+      snake: {parts:[p(1,1), p(2,1), p(3,1),p(4,1),p(4,2),p(4,3),p(3,3),p(2,3),p(1,3),p(0,3)]},
+      food: p(2,2),
+    },
+    {
+      name: "5x4 right in front of food trap",
+      snake: {parts:[p(1,2),p(1,1), p(2,1), p(3,1),p(4,1),p(4,2),p(4,3),p(3,3),p(2,3),p(1,3),p(0,3)]},
+      food: p(2,2),
+    },
+    {
+      name: "5x4 obvious trap",
+      snake: {parts:[p(1,1), p(2,1), p(3,1),p(4,1),p(4,2),p(4,3),p(3,3),p(2,3),p(1,3),p(0,3)]},
+      food: p(3,2),
+    },
+    // todo trap to food and no path to tail
+  ].map(({snake, food, name}, idx) => it(`${name} (${idx})`, () => {
+    // does not try and wrap around when that move is blocked by the snake itself
+    const path = pathfindGreedy(snake, food, 5, 4);
+    expect(path).toBeDefined();
+    expect(path.length).toBeGreaterThanOrEqual(1);
+    expect(path[path.length-1]).not.toEqual(expect.objectContaining(food));
+  }));
+
+});
