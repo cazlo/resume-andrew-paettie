9d159dfbcc10290bbff10ba6567dbe52bdd1cb10
Andrew Paettie <paettiea@gmail.com>
2018-11-18 23:51:30 -0600

    bring tests in line with reality

--- src/containers/SnakeGame/sagas/performance8x8.int.js
+++ src/containers/SnakeGame/sagas/performance8x8.int.js
@@ -4,5 +4,5 @@ import { toggleGreedyShortestPathToTail, toggleEnableAstar } from '../actions/ai
 // 12 % avg seems to be a bit better than current performance, so should be able to be used to spot
 // performance regressions
 performanceTest({ gamesToSimulate: 30, avgThreshold: 0.38, size:8, aiAction: toggleEnableAstar, name:"A star" });
-performanceTest({ gamesToSimulate: 30, avgThreshold: 0.38, size:8, aiAction: toggleGreedyShortestPathToTail, name:"Greedy SP->Tail" });
+performanceTest({ gamesToSimulate: 20, avgThreshold: 0.7, size:8, aiAction: toggleGreedyShortestPathToTail, name:"Greedy SP->Tail" });
 
