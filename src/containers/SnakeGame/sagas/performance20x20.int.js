27764d22f81ce171afaa4d3dd50872b16678bbc7
Andrew Paettie <paettiea@gmail.com>
2018-11-18 23:56:26 -0600

    delete annoying test

--- src/containers/SnakeGame/sagas/performance20x20.int.js
+++
@@ -1,8 +0,0 @@
-import { performanceTest } from './testHelper';
-import { toggleEnableAstar } from '../actions/aiConfigAction';
-
-// 12 % avg seems to be a bit better than current performance, so should be able to be used to spot
-// performance regressions
-performanceTest({ gamesToSimulate: 10, avgThreshold: 0.12, size:20, aiAction: toggleEnableAstar, name: "A star" });
-// performanceTest({ gamesToSimulate: 2, avgThreshold: 0.5, size:20, aiAction: toggleGreedyShortestPathToTail, name:"Greedy SP->Tail" });
-
