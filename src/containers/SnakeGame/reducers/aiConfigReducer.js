7c07b80a8b09c57116558a44b607ca22fda1267e
Andrew Paettie <paettiea@gmail.com>
2018-11-19 22:01:38 -0600

    make showing path configurable

--- src/containers/SnakeGame/reducers/aiConfigReducer.js
+++ src/containers/SnakeGame/reducers/aiConfigReducer.js
@@ -3,6 +3,7 @@ import { combineReducers, createReducer } from 'redux-starter-kit';
 
 const defaultSettings = {
   enableAI : false,
+  showPath: false,
   greedyShortestPathToTail: true,
   playerName: "SKYNET",
 };
@@ -11,6 +12,10 @@ export const enableAI = createReducer(defaultSettings.enableAI, {
   [Action.TOGGLE_ENABLE_A_STAR]: (state, action) => action.checked,
   [Action.TOGGLE_GREEDY_SP_TAIL]: (state, action) => action.checked ? false : state,
 });
+export const showPath = createReducer(defaultSettings.showPath, {
+  [Action.TOGGLE_SHOW_PATH]: (state, action) => action.checked,
+});
+
 export const greedyShortestPathToTail = createReducer(defaultSettings.greedyShortestPathToTail, {
   [Action.TOGGLE_GREEDY_SP_TAIL]: (state, action) => action.checked,
   [Action.TOGGLE_ENABLE_A_STAR]: (state, action) => action.checked ? false : state,
@@ -18,9 +23,9 @@ export const greedyShortestPathToTail = createReducer(defaultSettings.greedyShor
 export const playerName = createReducer(defaultSettings.playerName, {
   [Action.CHANGE_NAME]: (state, action) => action.playerName,
 });
-
 export default combineReducers({
   enableAI,
+  showPath,
   greedyShortestPathToTail,
   playerName,
 });
