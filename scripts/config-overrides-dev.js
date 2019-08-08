4d86b9d783baa4bd8fa45cc17dbe8b0949431fd9
Andrew Paettie <paettiea@gmail.com>
2019-08-07 20:52:42 -0700

    remove unused hack for launching this thing without ejecting

--- scripts/config-overrides-dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = function configOverrides(config) {
-  // Add the svg loader
-  const loaderList = config.module.rules[2].oneOf;
-  loaderList.splice(loaderList.length - 1, 0, {
-    test: /\.svg/,
-    loader: 'svg-inline-loader?removeTags',
-  });
-};
--- scripts/config-overrides-int.js
+++
@@ -1,7 +0,0 @@
-const configOverride = require("../integration-test.config");
-
-module.exports = function configOverrides(config) {
-  config.testMatch = configOverride.testMatch;
-  config.verbose = true;
-  return config;
-};
