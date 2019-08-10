f81f8fc67145d159fa677a0fddc72204f9e1f862
Drew Paettie <cazlo@users.noreply.github.com>
2019-08-09 19:26:06 -0700

    get react-snap issue fixed

---
+++ src/buildutil/noscript.js
@@ -0,0 +1,23 @@
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const noScriptContents = fs.readFileSync('build/noscript/index.html', 'utf8');
+const noScriptDocument = cheerio.load(noScriptContents);
+console.log('Parsed rendered DOM');
+const appNode = noScriptDocument('#root').html();
+// console.log(appNode);
+const style = noScriptDocument('style[data-jss]')
+  .map(function(i, el) {
+    return cheerio(el).html();
+  })
+  .get()
+  .join('\n');
+
+// console.log(style);
+const normalContents = fs.readFileSync('build/index.html', 'utf8');
+const normalIndex = cheerio.load(normalContents);
+normalIndex('#root').replaceWith(`<noscript>${appNode}<style>${style}</style></noscript>`);
+console.log('Writing to file');
+// console.log(normalIndex.html());
+fs.writeFileSync('build/index.html', normalIndex.html());
+console.log('noscript replace success!!!!');
