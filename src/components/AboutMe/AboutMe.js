3c5fca6582c562634b616827af0c90d7d1d770f7
Andrew Paettie <paettiea@gmail.com>
2021-08-08 23:42:44 -0700

    cleaner about me buttons
    no longer coliding when md down

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { Box, Typography } from '@material-ui/core';
+
 import PropTypes from 'prop-types';
 
 import selfImage from '../../data/me.jpg';
-
 import './AboutMe.css';
 import ScreenBlock from '../ScreenBlock/ScreenBlock';
 
 const Intro = ({ style }) => (
   <ScreenBlock id="Resume-aboutMe" className="ResumeAboutMeBlock" style={style}>
     <div id="" className="Resume-aboutMe container">
-      <div className="heading">
+      <Typography className="heading">
         <h2 className="ResumeAboutMe" style={{ color: '#fff' }}>
           About Me
         </h2>
         <p id="ResumeAboutMeSubtitle" className="ResumeAboutMeBlock-Subtitle">
           A small introduction about myself
         </p>
-      </div>
+      </Typography>
       <div className="ResumeAboutMeBlock-content">
-        <div className="ResumeAboutMeBlock-description">
+        <Typography className="ResumeAboutMeBlock-description">
           <h3>Andrew Paettie</h3>
           <h4>Senior Software Based Solutions Engineer</h4>
           <p className="Resume-summary" />I am an experienced and capable full stack software engineer with leadership
@@ -30,33 +31,46 @@ const Intro = ({ style }) => (
           contact me via one of the listed methods (serious inquiries only please).
           <br />
           <br />
-          <div className="ResumeAboutMeBlock-links">
-            <Button variant="contained" color="primary" target="_blank" href="/static/resume/resume-Andrew-Paettie.pdf">
-              Resume PDF
-            </Button>
-            &nbsp;&nbsp;&nbsp;
-            <Button
-              variant="contained"
-              color="primary"
-              target="_blank"
-              href="/static/resume/resume-Andrew-Paettie.docx"
-            >
-              Resume DOCX
-            </Button>
-            &nbsp;&nbsp;&nbsp;
-            <Button href="https://github.com/cazlo" variant="contained" color="primary">
-              Github
-            </Button>
-            &nbsp;&nbsp;&nbsp;
-            <Button href="https://www.linkedin.com/in/andrew-paettie/" variant="contained" color="primary">
-              LinkedIn
-            </Button>
-            &nbsp;&nbsp;&nbsp;
-            <Button href="mailto:paettiea@gmail.com" variant="contained" color="primary">
-              Email
-            </Button>
-          </div>
-        </div>
+          <Box display="flex" flexDirection="row" justifyContent="center">
+            <Box margin={1}>
+              <Button href="mailto:paettiea@gmail.com" variant="contained" color="primary">
+                Email
+              </Button>
+            </Box>
+            <Box margin={1}>
+              <Button href="https://github.com/cazlo" variant="contained" color="primary">
+                Github
+              </Button>
+            </Box>
+            <Box margin={1}>
+              <Button href="https://www.linkedin.com/in/andrew-paettie/" variant="contained" color="primary">
+                LinkedIn
+              </Button>
+            </Box>
+          </Box>
+          <Box display="flex" flexDirection="row" justifyContent="center">
+            <Box margin={1}>
+              <Button
+                variant="contained"
+                color="primary"
+                target="_blank"
+                href="/static/resume/resume-Andrew-Paettie.pdf"
+              >
+                Resume PDF
+              </Button>
+            </Box>
+            <Box margin={1}>
+              <Button
+                variant="contained"
+                color="primary"
+                target="_blank"
+                href="/static/resume/resume-Andrew-Paettie.docx"
+              >
+                Resume DOCX
+              </Button>
+            </Box>
+          </Box>
+        </Typography>
 
         <div className="ResumeAboutMeBlock-profilePicture Resume-profilePicture">
           <img alt="Andrew Paettie" src={selfImage} />
